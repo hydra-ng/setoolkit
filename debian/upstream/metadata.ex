0f3a925a698aa2c79d05bf4b991fe09b43b31b0d
joe <joe@rstrike.my.id>
2025-03-25 19:30:19 +0000

    Initial commit

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/setoolkit/issues
+# Bug-Submit: https://github.com/<user>/setoolkit/issues/new
+# Changelog: https://github.com/<user>/setoolkit/blob/master/CHANGES
+# Documentation: https://github.com/<user>/setoolkit/wiki
+# Repository-Browse: https://github.com/<user>/setoolkit
+# Repository: https://github.com/<user>/setoolkit.git
